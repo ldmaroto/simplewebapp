4e9ede6a2083849317318754bfa06670a192826d
Luis Diego Maroto Segura <ldmaroto@mac.com>
2020-04-05 15:24:05 -0600

    Configurado la carpeta de vistas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express'); // se convoca al modulo 'express' de nodejs.
 const path = require('path'); // Modulo necesario para trabajar rutas de archivos y directorios.
-
+const exphbs = require('express-handlebars');
 
 // SEC: Initializations
 const app = express(); // express es una función, que devuelve un objeto, el cual se almacena en una variable que se llama app.
@@ -9,7 +9,13 @@ const app = express(); // express es una función, que devuelve un objeto, el cu
 // SEC: Settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views')); // esta linea define la ubicación de la carpeta "views"
-
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // SEC: Middleware
 
