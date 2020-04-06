68948a624ffdbb7918e2a0a5d0fb7019460d64de
Luis Diego Maroto Segura <ldmaroto@mac.com>
2020-04-05 18:47:25 -0600

    Definido las rutas temporales, el servidor trabaja git add . !

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/users/singin', (req, res) => {
+    res.send('Ingresando a la app');
+})
+
+router.get('/users/singup', (req, res) => {
+    res.send('Formulario de autenticación');
+})
 
 module.exports = router;
